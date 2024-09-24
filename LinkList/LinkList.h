4b9b8c1f601477994a0e4efea4efd64e8fd0637d
yuanxmo <1764187345@qq.com>
2024-09-24 18:10:13 +0800

    init headNode and destroy linkList

--- LinkList/LinkList.h
+++ LinkList/LinkList.h
@@ -24,6 +24,7 @@ typedef struct Node {
     struct Node* next;
 } ListNode, *LinkList;
 
+
 /**
  * 以 ()->()->NULL 形式打印链表
  * 
@@ -38,6 +39,36 @@ void ListNode_print_Link(ListNode* listNode) {
         printf("%d ->", p->data);
     }
     printf("NULL\n");
+}
+
+/**
+ * 初始化头结点
+ * 
+ * @return 初始化完成的头结点指针(不确保成功)
+ */
+ListNode *ListNode_init() {
+    LinkList head = (LinkList)malloc(sizeof(ListNode));
+    if (head == NULL)
+        return head;
+    head->next = NULL;
+    return head;
+}
+
+/**
+ * 销毁链表
+ * 
+ * @param (ListNode*) listNode 链表结点指针
+ */
+void ListNode_destroy(ListNode** listNode) {
+    ListNode* p = (*listNode)->next;
+    ListNode* q = NULL;
+    while (p != NULL) {
+        q = p;
+        p = p->next;
+        free(q);
+    }
+    free(*listNode);
+    *listNode = NULL;
 }
 
 
